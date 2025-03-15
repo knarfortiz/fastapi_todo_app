aa0a1efd524260beef621454ced7a41b959d59f3
knarfortiz <o.g.knarf@hotmail.com>
2025-03-15 10:42:45 -0500

    add end todo all and once

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-from typing import Union
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Path
+from sqlalchemy.orm import Session
+from starlette import status
 
 import models
-from database import engine
+from database import SessionLocal, engine
+from models import Todos
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
+
+
+@app.get("/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+    return todo
