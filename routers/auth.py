d8101352bacbd59df59b05f0658aaaa5ba6d8d11
knarfortiz <o.g.knarf@hotmail.com>
2025-03-25 08:15:18 -0500

    hash user password

--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
+from passlib.context import CryptContext
 
 from models import Users
 from requests import CreateUserRequest
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 @router.get("/auth/")
@@ -18,7 +21,7 @@ async def create_user(user_request: CreateUserRequest):
         username=user_request.username,
         first_name=user_request.first_name,
         last_name=user_request.last_name,
-        hashed_password=user_request.password,
+        hashed_password=bcrypt_context.hash(user_request.password),
         role=user_request.role,
         is_active=True,
     )
