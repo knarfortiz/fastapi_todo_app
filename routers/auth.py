104978a9c7f4d75345c71ca464322632db0713b1
knarfortiz <o.g.knarf@hotmail.com>
2025-03-25 07:57:47 -0500

    add request and base endpoint for create user

--- routers/auth.py
+++ routers/auth.py
@@ -1,8 +1,26 @@
 from fastapi import APIRouter
 
+from models import Users
+from requests import CreateUserRequest
+
 router = APIRouter()
 
 
 @router.get("/auth/")
 async def get_user():
     return {"user": "authenticated"}
+
+
+@router.post("/auth/")
+async def create_user(user_request: CreateUserRequest):
+    user = Users(
+        email=user_request.email,
+        username=user_request.username,
+        first_name=user_request.first_name,
+        last_name=user_request.last_name,
+        hashed_password=user_request.password,
+        role=user_request.role,
+        is_active=True,
+    )
+
+    return user
