0664eb8560708db1f7bfb621c398a0cce9456c74
knarfortiz <o.g.knarf@hotmail.com>
2025-06-17 22:31:54 -0500

    add router admin

--- routers/admin.py
+++ routers/admin.py
@@ -1,11 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
 
 from database import db_dependency
 from models import Todos
-from requests import TodoRequest
 
 from .auth import get_current_user
 
@@ -19,4 +18,8 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @router.get("/todo", status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency, db: db_dependency):
+    if user is None or user["role"] != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
+        )
     return db.query(Todos).all()
