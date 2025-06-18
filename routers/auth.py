a9d6646742e52bdad6ba66935eaa5be886fadb26
knarfortiz <o.g.knarf@hotmail.com>
2025-06-17 19:04:22 -0500

    config base token

--- routers/auth.py
+++ routers/auth.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from starlette import status
@@ -23,6 +24,15 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+
+def authenticate_user(db: db_dependency, username: str, password: str):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
 
 
 @router.get("/auth")
@@ -48,6 +58,16 @@ async def create_user(db: db_dependency, user_request: CreateUserRequest):
 
     return user
 
+
 @router.post("/token")
-async def login_for_access_token():
-    return "token"
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(db, form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password"
+        )
+
+    return {"access_token": user.username, "token_type": "bearer"}
