f1fd4f84abddb2c915b66a25bb53ebf3cfdfcffb
knarfortiz <o.g.knarf@hotmail.com>
2025-06-17 21:20:33 -0500

    get todos list by users authenticate

--- routers/todos.py
+++ routers/todos.py
@@ -29,8 +29,8 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Todos).all()
+async def read_all(user: user_dependency, db: db_dependency):
+    return db.query(Todos).filter(Todos.owner_id == user["id"]).all()
 
 
 @router.get("/{todo_id}", status_code=status.HTTP_200_OK)
@@ -48,11 +48,6 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 async def create_todo(
     user: user_dependency, db: db_dependency, todo_request: TodoRequest
 ):
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
-        )
-    
     todo_model = Todos(**todo_request.model_dump(), owner_id=user["id"])
 
     db.add(todo_model)
