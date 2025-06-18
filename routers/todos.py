3ae8f0f1625c921caff5448b9e2054aa6d2de060
knarfortiz <o.g.knarf@hotmail.com>
2025-06-17 20:16:54 -0500

    create todo with login user

--- routers/todos.py
+++ routers/todos.py
@@ -8,6 +8,8 @@ from database import SessionLocal
 from models import Todos
 from requests import TodoRequest
 
+from .auth import get_current_user
+
 router = APIRouter(
     prefix="/todos",
     tags=["Todos"],
@@ -23,6 +25,7 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
@@ -42,11 +45,19 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.model_dump())
+async def create_todo(
+    user: user_dependency, db: db_dependency, todo_request: TodoRequest
+):
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
+        )
+    
+    todo_model = Todos(**todo_request.model_dump(), owner_id=user["id"])
 
     db.add(todo_model)
     db.commit()
+    db.refresh(todo_model)
 
     return todo_model
 
